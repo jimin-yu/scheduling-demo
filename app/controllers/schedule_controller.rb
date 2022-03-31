c0031378207ef6cd299cca78ff84106f66321997
jimin-yu <jjmmyyou111@deali.net>
2022-03-31 15:50:01 +0900

    feat: update

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -2,9 +2,20 @@
 # custom -> default 날짜에 disable / 혹은 시간 변경
 
 class ScheduleController < ApplicationController
+    before_action :set_schedule
 
-    def hello
-        render json: {message: 'hello'}
+    def update
+        @schedule.update!(schedule_params)
     end
-    
+
+    private
+
+    def set_schedule
+        @schedule = PurchaseSchedule.find_or_create_by(date: params.fetch(:date, Date.today))
+    end
+
+    def schedule_params
+        params.require(:purchase_schedule).permit(:time, :enabled)
+    end
+
 end
